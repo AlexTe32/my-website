9bdeee91b51a4792d4eb1fbdffd1f30dd2ff6fc5
AlexTe32 <trajcevskialeksandar861@gmail.com>
2025-05-27 18:22:12 +0200

    27.5.2025
    Added dragabal windowes for the pop ups

--- script.js
+++ script.js
@@ -6,12 +6,40 @@ function hideModal(id) {
     document.getElementById(id).style.display = "none";
 }
 
-// Optional: close modal on outside click
-window.onclick = function(event) {
-    const modals = document.querySelectorAll('.modal');
-    modals.forEach(modal => {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    });
+dragElement(document.getElementById("educationModal"));
+dragElement(document.getElementById("skillsModal"));
+dragElement(document.getElementById("projectsModal"));
+
+function dragElement(elmnt) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    // Make the whole element draggable
+    elmnt.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
